65f3a3970aea18199fc131b3d3579a951c5a3867
レ　クアン <le-quan@pdc.proto-g.co.jp>
2019-12-18 16:14:00 +0700

    finish demo

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,10 +4,14 @@ import hello.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.jws.WebParam;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HelloController {
@@ -23,5 +27,38 @@ public class HelloController {
         List<Blog> blogs = blogRepo.findAll();
         indexModel.addAttribute("blogs", blogs);
         return "index";
+    }
+
+
+    @RequestMapping(value = "/blog", method = RequestMethod.GET)
+    public String newBlog(Model model) {
+        model.addAttribute("pageTitle", "New Blog");
+        model.addAttribute("givenAction", "/blog");
+        model.addAttribute("givenTitle", "");
+        model.addAttribute("givenContent", "");
+        return "blog";
+    }
+
+    @RequestMapping(value = "/blog", method = RequestMethod.POST)
+    public String addBlog(@RequestParam String title, @RequestParam String content) {
+        Blog newBlog = new Blog(title, content);
+        blogRepo.save(newBlog);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/blog/{id}", method = RequestMethod.GET)
+    public String viewBlog(@PathVariable(value = "id") int id, Model model) {
+        Blog blog = blogRepo.findById(id).get();
+        model.addAttribute("pageTitle", "Blog detail");
+        model.addAttribute("givenAction", "/blog");
+        model.addAttribute("givenTitle", blog.getTitle());
+        model.addAttribute("givenContent", blog.getContent());
+        return "blog";
+    }
+
+    @RequestMapping(value = "/blog/delete/{id}", method = RequestMethod.GET)
+    public String deleteBlog(@PathVariable(value = "id") int id) {
+        blogRepo.deleteById(id);
+        return "redirect:/";
     }
 }
